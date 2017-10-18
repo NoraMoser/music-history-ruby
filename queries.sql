3c14c3d4eb371e070017d52c47e654004b9f2a56
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-10-18 09:14:11 -0500

    few more

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 SELECT * FROM genre;
 
 INSERT INTO Artist (ArtistId, ArtistName, YearEstablished) VALUES (NULL, "RadioHead", 1990)
+
+INSERT INTO Album (AlbumId, Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES (NULL, "Who Cares", "September 16", 1234, "NoraLabel", 23, 4);
+
+SELECT * FROM Album;
+
+INSERT INTO Song (SongId, Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES (NULL, "Nora is Awesome", 124, "September 16", 4, 23, 23);
